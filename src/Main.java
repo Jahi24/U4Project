ec7d338376be8b8cd025e69ab824f0725dcc7056
JahiDesormeaux-will <jahisaleem@gmail.com>
2026-02-06 08:44:17 -0500

    Fixed approach

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Poker hand1 = new Poker("3","2","10","3","King",765);
-        Poker hand2 = new Poker("10","5","5","Jack","5",684);
-        Poker hand3 = new Poker("King","King","6","7","7",28);
-        Poker hand4 = new Poker("King","10","Jack","Jack","10",220);
-        Poker hand5 = new Poker("Queen","Queen","Queen","Jack","Ace",483);
-        hand1.makeHand();
-        hand1.createRank();
-        hand2.makeHand();
-        hand2.createRank();
-        hand3.makeHand();
-        hand3.createRank();
-        hand4.makeHand();
-        hand4.createRank();
-        hand5.makeHand();
-        hand5.createRank();
+        StringBuilder fileData = new StringBuilder();
+        try {
+            File f = new File("src/data");
+            Scanner s = new Scanner(f);
 
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                fileData.append(line).append("\n");
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        String[] lines = fileData.toString().split("\n");
+        int numberOfLines = lines.length;
+        Poker[] allHands = new Poker[numberOfLines];
+        for (int i = 0; i < lines.length; i++) {         // for each line in the file, make a Poker object with all the proper info: allHands[i] = new Poker( ... )
+            String  currentHand = lines[i];
+            String[] cardsPart = currentHand.split("\\|");
+            String[] cards = cardsPart[0].split(",");
+            int bid = Integer.parseInt(cardsPart[1]);
+            String str1 = cards[0];
+            String str2 = cards[1];
+            String str3 = cards[2];
+            String str4 = cards[3];
+            String str5 = cards[4];
+            allHands[i] = new Poker(str1, str2, str3, str4, str5, bid);
+            allHands[i].makeHand();
+        }
         Poker.printInfo();
     }
 }
